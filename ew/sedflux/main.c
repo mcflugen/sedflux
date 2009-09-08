b7819fabb0d2fd2c0c864ccc3279bde07b9d173c
huttone <huttone@1ad62080-3021-0410-8de1-bda6899d766e>
2009-09-08 23:49:08 +0000

    * Rewrite main application to use new sedflux API.  Output (elevation) is now   written as BOV files.  This output is controlled by the application and does   not change how any output is created through the sedflux 'data dump' or   'measuring station' processes.

--- ew/sedflux/main.c
+++ ew/sedflux/main.c
@@ -28,7 +28,107 @@
 #include <utils/utils.h>
 #include <sed/sed_sedflux.h>
 #include "sedflux.h"
+#include "sedflux_api.h"
 
+void
+print_sedflux_var (Sedflux_state* s, const gchar* prefix, const gchar* val_s)
+{
+  eh_require (s);
+  eh_require (prefix);
+  eh_require (val_s);
+
+  {
+    double* z;
+    const int nx = sedflux_get_nx (s);
+    const int ny = sedflux_get_ny (s);
+    gint len[3] = {1, nx, ny};
+    double size[3] = {1, 1, 1};
+
+    //z = sedflux_get_value (s, val_s, len);
+    z = sedflux_get_value_cube (s, val_s, len);
+
+    eh_bov_print (prefix, z, val_s, len, size, NULL);
+
+    eh_free (z);
+  }
+}
+
+int
+main (int argc, char *argv[])
+{
+  GError* error       = NULL;
+  gchar*  command_str = NULL;
+  gchar* init_file = NULL;
+  gchar* work_dir = NULL;
+  gchar* run_desc = NULL;
+  gint dimen = 0;
+
+  g_thread_init (NULL);
+  eh_init_glib ();
+  g_log_set_handler (NULL, G_LOG_LEVEL_MASK, &eh_logger, NULL);
+
+  { /* Parse command line arguments */
+    Sedflux_param_st* p = NULL;
+
+    command_str = eh_render_command_str (argc, argv);
+
+    p = sedflux_parse_command_line (argc, argv, &error);
+    eh_exit_on_error( error , "Error parsing command line arguments" );
+
+    init_file = g_strdup (p->init_file);
+    work_dir = g_strdup (p->working_dir);
+    run_desc = g_strdup (p->run_desc);
+    dimen = p->mode_2d?(2):(3);
+
+    eh_free( p );
+  }
+
+  { /* Create the project directory and check permissions */
+    sedflux_setup_project_dir( &init_file , &work_dir , &error );
+    eh_exit_on_error( error , "Error setting up project directory" );
+
+    sedflux_print_info_file (init_file, work_dir, command_str, run_desc);
+  }
+
+   /* Setup the signal handling */
+  sed_signal_set_action();
+
+  { /* Initialze sedflux and then run it. */
+    Sedflux_state* state = sedflux_initialize (init_file, dimen);
+
+    if (state)
+    {
+      double start = sedflux_get_start_time (state);
+      double end = sedflux_get_end_time (state);
+      double dt = (end-start)/100;
+      double t;
+      gchar* prefix = NULL;
+      int i;
+
+      for (t=start+dt, i=0; t<end; t+=dt, i++)
+      {
+        sedflux_run_until (state, t);
+
+        prefix = g_strdup_printf ("%s_%.4d","grain",i);
+        //print_sedflux_var (state, prefix, "Elevation");
+        print_sedflux_var (state, prefix, "grain");
+
+        eh_free (prefix);
+      }
+    }
+
+    sedflux_finalize (state);
+  }
+
+  if (g_getenv("SED_MEM_CHECK"))
+    eh_heap_dump( "heap_dump.txt" );
+
+  eh_exit (EXIT_SUCCESS);
+
+  return EXIT_SUCCESS;
+}
+
+#if defined(OLD_WAY)
 int
 main( int argc , char *argv[] )
 {
@@ -68,5 +168,5 @@ main( int argc , char *argv[] )
 
    return EXIT_SUCCESS;
 }
-
+#endif
 
