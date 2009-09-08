60770aa44dc7f3002cf2af591746dd5e5bdd01d9
huttone <huttone@1ad62080-3021-0410-8de1-bda6899d766e>
2009-09-08 22:48:48 +0000

    * Add files subside_api.[ch].     Create an API for the subsidence model.  This provides functions that     create, modify, and query a subsidence state variable.  The state variable     is opaque to the programmer using the API.
        The API also provides Initialize, Run, and Finalize methods for the 
        model.

---
+++ ew/subside/subside_api.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <utils/utils.h>
+#include "subside_api.h"
+
+struct Subside_state
+{
+  Eh_dbl_grid z; //< Elevation (m)
+  Eh_dbl_grid load; //< Overlying load
+  double eet; //< Effective elastic thickness (m)
+  double youngs; //< Young's modulus
+  double relaxation; //< Relaxation time (y)
+  double rho_w; //< Density of water (kg/m3)
+  double rho_m; //< Density of mantle (kg/m3)
+
+  double time; //< The current time (y)
+};
+
+Subside_state*
+sub_init (int nx, int ny, double dx, double dy)
+{
+  Subside_state* state = g_new (Subside_state, 1);
+
+  { /* Set default constants */
+    state->eet = 5000.;
+    state->youngs = 7e10;
+    state->relaxation = 5000.;
+    state->rho_w = 1030.;
+    state->rho_m = 3300.;
+
+    state->time = 0;
+  }
+
+  { /* Set up the initial profile */
+    Eh_dbl_grid z = eh_grid_new (double, nx, ny);
+
+    eh_grid_set_x_lin (z, 0, dx);
+    eh_grid_set_y_lin (z, 0, dy);
+
+    eh_dbl_grid_set (z, 0.);
+
+    state->z = z;
+  }
+
+  { /* Set up the initial load */
+    Eh_dbl_grid v = eh_grid_new (double, nx, ny);
+
+    eh_grid_set_x_lin (v, 0, dx);
+    eh_grid_set_y_lin (v, 0, dy);
+
+    eh_dbl_grid_set (v, 0.);
+
+    state->load = v;
+  }
+
+  return state;
+}
+
+void
+sub_run (Subside_state* s, double t)
+{
+  subside_grid_load(s->z, s->load, s->eet, s->youngs);
+  s->time = t;
+}
+
+void
+sub_destroy (Subside_state* state)
+{
+  if (state)
+  {
+    eh_grid_destroy (state->z, TRUE);
+    g_free (state);
+  }
+}
+
+const double*
+sub_get_deflection (Subside_state* state)
+{
+  eh_return_val_if_fail (state, NULL);
+  return eh_dbl_grid_data_start (state->z);
+}
+
+const double*
+sub_get_load (Subside_state* state)
+{
+  eh_return_val_if_fail (state, NULL);
+  return eh_dbl_grid_data_start (state->load);
+}
+
+const double*
+sub_get_x (Subside_state* state)
+{
+  eh_return_val_if_fail (state, NULL);
+  return eh_grid_x (state->z);
+}
+
+const double*
+sub_get_y (Subside_state* state)
+{
+  eh_return_val_if_fail (state, NULL);
+  return eh_grid_y (state->z);
+}
+
+int
+sub_get_nx (Subside_state* state)
+{
+  eh_return_val_if_fail (state, 0);
+  return eh_grid_n_x (state->z);
+}
+
+int
+sub_get_ny (Subside_state* state)
+{
+  eh_return_val_if_fail (state, 0);
+  return eh_grid_n_y (state->z);
+}
+
+void
+sub_set_load (Subside_state* state, double* load)
+{
+  eh_return_if_fail (state);
+  eh_grid_borrow_data (state->load, (void*)load);
+}
+
+void
+sub_set_load_at (Subside_state* state, double load, int i, int j)
+{
+  eh_return_if_fail (state);
+  
+  eh_dbl_grid_set_val (state->load, i, j, load);
+}
+
+void
+sub_set_eet (Subside_state* state, double new_val)
+{
+  eh_return_if_fail (state);
+  state->eet = new_val;
+}
+
+void
+sub_set_youngs (Subside_state* state, double new_val)
+{
+  eh_return_if_fail (state);
+  state->youngs = new_val;
+}
+
+void
+sub_set_relax_time (Subside_state* state, double new_val)
+{
+  eh_return_if_fail (state);
+  state->relaxation = new_val;
+}
+
---
+++ ew/subside/subside_api.h
@@ -0,0 +1,57 @@
+#if !defined (SUBSIDENCE_API_H)
+#define SUBSIDENCE_API_H
+
+/** Subidence model state variable
+
+This is a completely opaque data structure that holds the state of a
+subsidence model.
+
+*/
+typedef struct Subside_state Subside_state;
+
+/** Create and initialize a new Subside_state
+
+@param nx Number of x nodes in computation grid
+@param ny Number of y nodes in computation grid
+@param dx Width of cells in the x direction
+@param dy Width of cells in the y direction
+
+@return A newly allocated Subside_state
+
+*/
+Subside_state* sub_init (int nx,int ny,double dx,double dy);
+
+/** Advance a Subside_state forward in time.
+
+@param s A Subside_state
+@param t Time (years) to advance model to.
+*/
+void sub_run (Subside_state* s, double t);
+
+/** Destroy a Subside_state
+
+@param s A Subside_state
+*/
+void sub_destroy (Subside_state* s);
+
+/** Get deflection data
+
+@param s A Subside_state
+
+@return Array of deflections (in m)
+*/
+const double* sub_get_deflection (Subside_state* s);
+const double* sub_get_load (Subside_state*);
+const double* sub_get_x (Subside_state*);
+const double* sub_get_y (Subside_state*);
+
+int sub_get_nx (Subside_state*);
+int sub_get_ny (Subside_state*);
+
+void sub_set_load (Subside_state*, double*);
+void sub_set_eet (Subside_state*, double);
+void sub_set_youngs (Subside_state*, double);
+void sub_set_relax_time (Subside_state*, double);
+
+#endif
+
