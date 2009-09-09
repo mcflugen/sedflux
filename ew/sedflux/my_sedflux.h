4504c1ec77fe4491cfed41c4041b5451e411ec83
huttone <huttone@1ad62080-3021-0410-8de1-bda6899d766e>
2009-09-09 00:57:31 +0000

    * Remove extern declarations of my_proc_defs, my_proc_family as they are now   declared in sedflux_api.c. * Remove extern declaration of my_proc_checks as it's no longer used.

--- ew/sedflux/my_sedflux.h
+++ ew/sedflux/my_sedflux.h
@@ -11,11 +11,13 @@ sedflux will cycle through the process in the order that they are listed here.
 To add a new process to sedflux, you will need to add an entry to this list.
 sedflux will will then automatically cycle through your process.
 */
+/*
 extern Sed_process_init_t my_proc_defs[];
 
 extern Sed_process_family my_proc_family[];
 
 extern Sed_process_check my_proc_checks[];
+*/
 
 #endif /* MY_SEDFLUX_H */
 
