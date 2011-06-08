bddeb09a1520d6911e5215d924571f2772cddf6d
huttone <huttone@1ad62080-3021-0410-8de1-bda6899d766e>
2011-06-08 21:05:52 +0000

    * Set LibUtils version number to SEDFLUX_VERSION.

--- ew/utils/utils.pc.cmake
+++ ew/utils/utils.pc.cmake
@@ -1,7 +1,7 @@
 Name: LibUtils
 Description: My utility library
-Version: ${LIBUTILS_VERSION}
-Requires: glib-2.0 >= 2.0
+Version: ${SEDFLUX_VERSION}
+Requires: glib-2.0 >= 2.25
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lutils -lm
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
 
