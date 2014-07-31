522939a206eac84874183bb31f89b1406a574294
mcflugen <mcflugen@gmail.com>
2014-07-30 20:44:46 -0600

    Removed version requirement for sedflux utils.

--- ew/plume/plume.pc.cmake
+++ ew/plume/plume.pc.cmake
@@ -1,7 +1,7 @@
 Name: LibPlume
 Description: Plume library
 Version: ${PLUME_VERSION}
-Requires: glib-2.0 >= 2.25, utils >= 2.0, sed >= 2.0
+Requires: glib-2.0 >= 2.25, utils, sed >= 2.0
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmiplume
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0 -I${CMAKE_INSTALL_PREFIX}/include
 
--- ew/sedflux/sedflux3d.pc.cmake
+++ ew/sedflux/sedflux3d.pc.cmake
@@ -1,7 +1,7 @@
 Name: LibSed
 Description: Sedflux library
 Version: ${SEDFLUX_VERSION}
-Requires: glib-2.0 >= 2.25, utils >= 2.0, sed >= 2.0
+Requires: glib-2.0 >= 2.25, utils, sed >= 2.0
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmisedflux3d
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0
 
