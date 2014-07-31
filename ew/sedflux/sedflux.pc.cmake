befd4538820a559136cfa8e9153c43b7a1887212
mcflugen <mcflugen@gmail.com>
2014-07-30 21:10:44 -0600

    Removed version requirement for sedflux library.

--- ew/sedflux/sedflux.pc.cmake
+++ ew/sedflux/sedflux.pc.cmake
@@ -1,7 +1,7 @@
 Name: LibSed
 Description: Sedflux library
 Version: ${SEDFLUX_VERSION}
-Requires: glib-2.0 >= 2.25, utils, sed >= 2.0
+Requires: glib-2.0 >= 2.25, utils, sed
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lsedflux-2.0
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0
 
