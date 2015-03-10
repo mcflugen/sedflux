c4f8e720675629fe7d9c0ccd6f3d7599fa5fbcb4
mcflugen <mcflugen@gmail.com>
2015-03-10 11:50:40 -0600

    Update for new bmi_avulsion library.

--- ew/avulsion/avulsion.pc.cmake
+++ ew/avulsion/avulsion.pc.cmake
@@ -2,6 +2,6 @@ Name: LibAvulsion
 Description: Avulsion library
 Version: ${AVULSION_VERSION}
 Requires: glib-2.0 >= 2.25, utils, sed
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lavulsion
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_avulsion
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0 -I${CMAKE_INSTALL_PREFIX}/include
 
