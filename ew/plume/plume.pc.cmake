8fce8a4f84fbd6745d93c83bdc2e9df4bedc1c71
mcflugen <mcflugen@gmail.com>
2015-03-10 11:42:52 -0600

    Update for new BMI plume library.

--- ew/plume/plume.pc.cmake
+++ ew/plume/plume.pc.cmake
@@ -1,7 +1,7 @@
 Name: LibPlume
-Description: Plume library
+Description: Plume BMI library
 Version: ${PLUME_VERSION}
 Requires: glib-2.0 >= 2.25, utils, sed
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmiplume
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_plume
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0 -I${CMAKE_INSTALL_PREFIX}/include
 
