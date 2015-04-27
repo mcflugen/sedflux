bb1681e1a1f98f90542b40a41a73e9a0784737f5
mcflugen <huttone@colorado.edu>
2015-04-27 12:02:46 -0600

    Updated names of bmi libraries.

--- ew/sedflux/sedflux3d.pc.cmake
+++ ew/sedflux/sedflux3d.pc.cmake
@@ -2,6 +2,6 @@ Name: LibSed
 Description: Sedflux library
 Version: ${SEDFLUX_VERSION}
 Requires: glib-2.0 >= 2.25, utils, sed
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmisedflux3d
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_sedflux3d
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0 -I${CMAKE_INSTALL_PREFIX}/include
 
