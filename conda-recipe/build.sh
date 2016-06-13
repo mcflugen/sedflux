1a2bbb42ff723dc35c3115f09ac55d2092204537
mcflugen <mcflugen@gmail.com>
2016-06-13 13:15:05 -0600

    Removed make test step.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,5 +1,8 @@
+(cd /usr/local && mv bin bin.hide && mv lib lib.hide && mv include include.hide)
+
 mkdir _build && cd _build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 make all -j4
-make test
 make install
+
+(cd /usr/local && mv bin.hide bin && mv lib.hide lib && mv include.hide include)
