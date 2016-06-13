26b7cc1fd9fbe24950ef4b72e6cd0b89cf5c1660
mcflugen <mcflugen@gmail.com>
2016-06-13 14:48:09 -0600

    Updated secure token.

--- .ci/travis/deploy_to_anaconda.sh
+++ .ci/travis/deploy_to_anaconda.sh
@@ -5,6 +5,7 @@ else
 fi
 
 echo "Uploading to $CHANNEL"
+echo anaconda -t $ANACONDA_TOKEN upload --force --user csdms --channel $CHANNEL $HOME/miniconda/conda-bld/**/sedflux*bz2
 anaconda -t $ANACONDA_TOKEN upload --force --user csdms --channel $CHANNEL $HOME/miniconda/conda-bld/**/sedflux*bz2
 
 echo "Done."
