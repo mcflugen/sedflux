986aa0caec2606a289e40d87b9c6bbd0d8f25fe8
mcflugen <mcflugen@gmail.com>
2016-06-13 13:32:12 -0600

    Changed to hide /usr/local only on Darwin.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,8 +1,12 @@
-(cd /usr/local && mv bin bin.hide && mv lib lib.hide && mv include include.hide)
+if [ `uname` == 'Darwin' ]; then
+  (cd /usr/local && mv bin bin.hide && mv lib lib.hide && mv include include.hide)
+fi
 
 mkdir _build && cd _build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 make all -j4
 make install
 
-(cd /usr/local && mv bin.hide bin && mv lib.hide lib && mv include.hide include)
+if [ `uname` == 'Darwin' ]; then
+  (cd /usr/local && mv bin.hide bin && mv lib.hide lib && mv include.hide include)
+fi
