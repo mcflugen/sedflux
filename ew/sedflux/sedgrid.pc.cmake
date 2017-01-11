ba2a17bf88e8f810a52c911a35ca6538eccacc3c
mcflugen <mcflugen@gmail.com>
2017-01-11 15:27:43 -0700

    Add pkgconfig file for sedgrid.

---
+++ ew/sedflux/sedgrid.pc.cmake
@@ -0,0 +1,7 @@
+Name: LibSed
+Description: Sedflux library
+Version: ${SEDFLUX_VERSION}
+Requires: glib-2.0 >= 2.25, utils, sed
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_sedgrid
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include/ew-2.0 -I${CMAKE_INSTALL_PREFIX}/include
+
